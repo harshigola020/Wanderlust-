9702360fd86cbb5cd81a15e7db4a1020b8fb76fa
Harshita Gola <93837021+harshigola020@users.noreply.github.com>
2025-10-23 15:17:26 +0530

    Added Map for listing location and improved UI

---
+++ middleware.js
@@ -0,0 +1,65 @@
+const Listing = require("./models/listing");
+const Review = require("./models/review");
+const {ListingSchema} = require('./schema.js');
+const {reviewSchema} = require('./schema.js');
+const ExpressError= require("./util/ExpressError.js");
+
+module.exports.isLoggedIn= (req,res,next)=>{
+    if(!req.isAuthenticated()){
+        req.session.redirectUrl = req.originalUrl;
+        req.flash("error","Login required!");
+        return res.redirect("/login");
+    }
+    next();
+};
+
+module.exports.saveRedirectUrl= (req,res,next)=>{
+    if(req.session.redirectUrl){
+        res.locals.redirectUrl = req.session.redirectUrl;
+    }
+    next();
+};
+
+module.exports.isOwner= async (req,res,next)=>{
+    let {id} = req.params;
+    let listing= await Listing.findById(id);
+    if(!listing.owner.equals(res.locals.currUser._id)){
+        req.flash("error", "Action denied — You must be the owner to edit/delete!");
+        return res.redirect(`/listings/${id}`);
+    }
+    next();
+}
+
+//verify review author
+module.exports.isReviewAuthor= async (req,res,next)=>{
+    let {id,reviewId} = req.params;
+    let review= await Review.findById(reviewId);
+    if(!review.author.equals(res.locals.currUser._id)){
+        req.flash("error", "Action denied — You must be the author to delete the review!");
+        return res.redirect(`/listings/${id}`);
+    }
+    next();
+}
+
+
+//validation
+module.exports.validateListing=(req,res,next)=>{
+    let {error} = ListingSchema.validate(req.body);
+    if(error){
+        let errMsg= error.details.map((el)=>el.message).join(",");
+        throw new ExpressError(400,errMsg);
+    } else{
+        next();
+    }
+}
+
+//Validating new Review
+module.exports.validateNewReview=(req,res,next)=>{
+    let {error} = reviewSchema.validate(req.body);
+    if(error){
+        let errMsg= error.details.map((el)=>el.message).join(",");
+        throw new ExpressError(400,errMsg);
+    } else{
+        next();
+    }
+}
